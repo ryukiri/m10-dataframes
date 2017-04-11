84d9907d5c25f17c533b9446cdbb6f385ab5817c
Austin Quach <austinquachh@gmail.com>
2017-04-11 15:15:07 -0700

    exercise 4

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,23 +2,24 @@
 
 # Read data into a variable called `grants` using the `read.csv` function
 # Make sure *not* to read the strings as factors
+grants <- read.csv('gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at your data
-
+View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending <- sum(grants$total_amount)
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
-
+org <- grants$organization
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
 # What was the mean grant value?
-
+mean.Grant <- mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
 
