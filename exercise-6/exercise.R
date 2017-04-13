e61c5d626f747f35b3dc7f53fa45b9253b90202e
Austin Quach <austinquachh@gmail.com>
2017-04-12 18:20:02 -0700

    Exercise 6

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,22 +1,24 @@
 # Exercise 6: Husky Football 2016 Season
 # Read in the Husky Football 2016 game data into a variable called `husky.games.2016`
+husky.games.2016 <- read.csv('data/huskies_2016.csv')
 
 # Create a vector of the teams that the Huskies played against during that season
 # Call this vector `not.huskies`. You'll need to convert this column to a vector
-
+not.huskies <- as.vector(husky.games.2016['opponent'])
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-
+husky.scores <- as.vector(husky.games.2016['uw_score'])
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
+rushing.yards <- as.vector(husky.games.2016['rushing_yards'])
+passing.yards <- as.vector(husky.games.2016['passing_yards'])
 
 # Create a variabled called `combined.yards` that is the total yardage of the Huskies for each game
-
+combined.yards <- rushing.yards + passing.yards
 
 # What is the score of the game where the Huskies had the most combined yards?
-
+score.most.combined.yards <-
 
 # Write a function `MostYardsScore` that takes in a dataframe parameter `games` and returns a descriptive sentence
 # about the game that was played that had the most yards scored in it.
